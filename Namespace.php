8ee7576f9b04f9de1f0cfcae40d3893d88d7265f
Jerico Tolentino <jerico.t@uprinting.com>
2012-10-06 14:35:02 +0800

    Added PHP script

---
+++ Namespace.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dummy_Class
+{
+    function run()
+    {
+        echo 'Running from namespace!';
+    }
+
+    function db()
+    {
+        echo 'Connecting to database!';
+    }
+}
+
+?>
